39ee0265a303a8868bcdce35b2078bee73945db9
Danny Kulas <danny.kulas1@gmail.com>
2015-01-09 10:42:55 -0500

    created user model with devise

--- jokester/spec/models/user_spec.rb
+++ jokester/spec/models/user_spec.rb
@@ -1,20 +1,5 @@
-require "rails_helper"
+require 'rails_helper'
 
-describe User do
-	describe "::new" do
-		it "can be instantiated" do
-			u = User.new
-			expect(u.class).to eq(User)
-		end
-	end
-
-	# it { should validate_presence_of(:name)} 
-	# it { should validate_presence_of()} #email?
-	# it { should validate_presence_of()} #password?
-	# it { should validate_presence_of()} #confirm password?
-
-	it { should have_many(:jokes)}
-	it { should have_many(:comments)}
-
+RSpec.describe User, :type => :model do
+  pending "add some examples to (or delete) #{__FILE__}"
 end
-
