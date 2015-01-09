5f839fd36eae7c355df4fedde49418cf91c21c37
James Corey <mmjames04@yahoo.com>
2015-01-09 11:27:23 -0500

    added jokes column to comments

--- jokester/db/schema.rb
+++ jokester/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150109162436) do
+ActiveRecord::Schema.define(version: 20150109162654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,10 @@ ActiveRecord::Schema.define(version: 20150109162436) do
   create_table "comments", force: :cascade do |t|
     t.string  "body"
     t.integer "user_id"
+    t.integer "joke_id"
   end
 
+  add_index "comments", ["joke_id"], name: "index_comments_on_joke_id", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "jokes", force: :cascade do |t|
@@ -49,6 +51,7 @@ ActiveRecord::Schema.define(version: 20150109162436) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "comments", "jokes"
   add_foreign_key "comments", "users"
   add_foreign_key "jokes", "users"
 end
