26b7a02ebbe05409cee2cebc47a05477364ba759
Danny Kulas <danny.kulas1@gmail.com>
2015-01-09 15:39:37 -0500

    made some changes

--- jokester/app/controllers/users_controller.rb
+++ jokester/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-	# before_action :authenticate_user!
+	before_action :authenticate_user!
 
 	def index
 		@users = User.all
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			redirect_to @user
+			redirect_to jokes_path
 		else
 			redirect_to 'new'
 		end
