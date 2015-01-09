bf545ff2b6c7730223ff5157ba4e55f4650e183c
Danny Kulas <danny.kulas1@gmail.com>
2015-01-09 12:23:14 -0500

    crud functions

--- jokester/app/controllers/users_controller.rb
+++ jokester/app/controllers/users_controller.rb
@@ -2,5 +2,36 @@ class UsersController < ApplicationController
 
 	before_action :authenticate_user!
 
-	
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+		@jokes = @user.jokes
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to @user
+		else
+			redirect_to 'new'
+		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:email, :password)
+	end
+
 end
